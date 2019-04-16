49e377cbd3d5b009b44b2a3b69910d1be80657ed
rubiconRiver <garett.gelsinger@gmail.com>
2019-04-15 21:53:10 -0600

    added posts component

---
+++ src/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function Posts(props){
+
+
+    return(
+
+        <div style={{padding: '10px'}}>
+        <div>Id: {props.id}</div>
+        <div>name: {props.name}</div>
+      </div>
+
+    )
+}
